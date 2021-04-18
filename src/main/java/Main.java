4c12247a349467a4cd5403689077cc90189c9c21
dtiern5 <dtierney@fastmail.com>
2021-04-18 09:26:17 -0500

    added Store class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,19 @@
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         // Test your app here
+        Warehouse warehouse = new Warehouse();
+        warehouse.addProduct("coffee", 5, 10);
+        warehouse.addProduct("milk", 3, 20);
+        warehouse.addProduct("cream", 2, 55);
+        warehouse.addProduct("bread", 7, 8);
 
-        ShoppingCart cart = new ShoppingCart();
-        cart.add("milk", 3);
-        cart.print();
-        System.out.println("cart price: " + cart.price() + "\n");
+        Scanner scanner = new Scanner(System.in);
 
-        cart.add("buttermilk", 2);
-        cart.print();
-        System.out.println("cart price: " + cart.price() + "\n");
-
-        cart.add("milk", 3);
-        cart.print();
-        System.out.println("cart price: " + cart.price() + "\n");
-
-        cart.add("milk", 3);
-        cart.print();
-        System.out.println("cart price: " + cart.price() + "\n");
+        Store store = new Store(warehouse, scanner);
+        store.shop("John");
     }
 }
